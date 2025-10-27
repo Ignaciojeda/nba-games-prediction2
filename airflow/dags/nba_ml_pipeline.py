4651ea4a549ef707dc5c9d628b07807e441947eb
Ignaciojeda <ig.ojedao@duocuc.cl>
2025-10-27 12:03:37 -0300

    Airflow

--- airflow/dags/nba_ml_pipeline.py
+++ airflow/dags/nba_ml_pipeline.py
@@ -1,5 +1,3 @@
-# Crear el archivo del DAG en airflow\dags\
-@'
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
@@ -19,41 +17,21 @@ with DAG(
     default_args=default_args,
     description='NBA Machine Learning Pipeline with Kedro and DVC',
     schedule_interval=timedelta(days=1),
-    catchup=False
+    catchup=False,
+    tags=['nba', 'ml', 'kedro']
 ) as dag:
 
     # 1. Data Processing
     data_processing = BashOperator(
         task_id='data_processing',
-        bash_command='cd /opt/airflow/nba_project && kedro run --pipeline=data_engineering',
+        bash_command='cd /opt/airflow/nba_project && python -m kedro run',
     )
 
-    # 2. Feature Engineering
-    feature_engineering = BashOperator(
-        task_id='feature_engineering',
-        bash_command='cd /opt/airflow/nba_project && kedro run --pipeline=feature_engineering',
-    )
-
-    # 3. Classification Pipeline
-    classification = BashOperator(
-        task_id='classification_pipeline',
-        bash_command='cd /opt/airflow/nba_project && kedro run --pipeline=classification',
-    )
-
-    # 4. Regression Pipeline
-    regression = BashOperator(
-        task_id='regression_pipeline',
-        bash_command='cd /opt/airflow/nba_project && kedro run --pipeline=regression',
-    )
-
-    # 5. DVC Versioning
+    # 2. DVC Versioning
     dvc_versioning = BashOperator(
         task_id='dvc_versioning',
         bash_command='cd /opt/airflow/nba_project && dvc repro',
     )
 
     # Dependencies
-    data_processing >> feature_engineering
-    feature_engineering >> [classification, regression]
-    [classification, regression] >> dvc_versioning
-'@ | Out-File -FilePath "airflow\dags\nba_ml_pipeline.py" -Encoding UTF8
+    data_processing >> dvc_versioning
